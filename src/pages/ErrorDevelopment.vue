6b68e8840be732f4d3034f9db396a86aeb62166e
Tehcam <tehcam42@gmail.com>
2022-06-20 22:37:59 +0200

    update development page

--- src/pages/ErrorDevelopment.vue
+++ src/pages/ErrorDevelopment.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>This page is now in development...</div>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        418
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        Cette page est actuellement en développement !
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
